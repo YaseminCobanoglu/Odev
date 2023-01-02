2bb9e704be0b016147b5740c0e690faaa9a3ea79
Yasemin ÇOBANOĞLU <yasemin.cobanoglu@baogrenci.com>
2023-01-02 10:37:52 +0300

    test

---
+++ CA_Ornek/CA_Ornek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CA_Ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
